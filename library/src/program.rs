ac53f3d878dc296d402011b77ad253f6c5a33ff2
Matthew Klein <96837318+matthew-a-klein@users.noreply.github.com>
2026-03-12 10:35:53 +0000

    Set language and main struct attributes of the outputted LLZK module

--- library/src/program.rs
+++ library/src/program.rs
@@ -1,10 +1,15 @@
 //! Compile the outer level `Program` to LLZK `Module`
 
 use acir::{FieldElement, circuit::Program};
-use llzk::prelude::{BlockLike, LlzkContext, LlzkError, Location, Module, llzk_module};
+use llzk::prelude::{
+    BlockLike, FlatSymbolRefAttribute, LlzkContext, LlzkError, Location, Module, OperationMutLike,
+    StringAttribute, StructType, TypeAttribute, llzk_module,
+};
+use llzk_sys::{LANG_ATTR_NAME, MAIN_ATTR_NAME};
 
 use crate::circuit::translate_circuit;
 
+const MAIN_STRUCT_NAME: &str = "Circuit0";
 /// Translates an ACIR `Program` into an LLZK `Module`.
 ///
 /// Creates the root `module attributes {llzk.lang = "noir"}` and calls
@@ -14,7 +19,19 @@ pub fn translate_program<'c>(
     program: &Program<FieldElement>,
 ) -> Result<Module<'c>, LlzkError> {
     let location = Location::unknown(context);
-    let module = llzk_module(location);
+    let mut module = llzk_module(location);
+    module.as_operation_mut().set_attribute(
+        MAIN_ATTR_NAME.as_ref(),
+        TypeAttribute::new(
+            StructType::new(FlatSymbolRefAttribute::new(context, MAIN_STRUCT_NAME), &[]).into(),
+        )
+        .into(),
+    );
+
+    module.as_operation_mut().set_attribute(
+        LANG_ATTR_NAME.as_ref(),
+        StringAttribute::new(context, "ACIR").into(),
+    );
 
     for (i, circuit) in program.functions.iter().enumerate() {
         let struct_def = translate_circuit(context, circuit, i)?;
